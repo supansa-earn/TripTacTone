37e33c6f834695f99f74ff8c11edd3ae67068a04
Earn <earnandearn25@outlook.com>
2023-01-06 01:52:06 +0700

    handle auth

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,7 +12,24 @@
 
 </style>
 <script>
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { auth } from "../plugins/firebase";
+let currentUser = null
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    currentUser = {
+      username: user.displayName,
+      accessToken: user.accessToken,
+      uid: user.uid,
+      email: user.email,
+      photoURL: user.photoURL,
+    };
+  }
+});
 export default {
-
+  beforeMount() {
+    this.$store.commit("ON_AUTH_STATE_CHANGED_MUTATION", currentUser);
+  
+  },
 }
 </script>
--- store/index.js
+++ store/index.js
@@ -5,5 +5,7 @@ export const state = () => ({
 export const mutations = {
   ON_AUTH_STATE_CHANGED_MUTATION: (state, user) => {
       state.user = user;
+      console.log(state.user)
   },
+
 };
