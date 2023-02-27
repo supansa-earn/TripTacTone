c3cc5befd8208a40cbd9e00281f7ab4a643b36ac
Earn <earnandearn25@outlook.com>
2023-02-27 13:58:58 +0700

    try somethings

--- api/cafe/index.js
+++ api/cafe/index.js
@@ -13,6 +13,14 @@ const getCafes = async (id=null) => {
 }
 
 const createCafes = async () => {
+  axios.post('/login', {
+    dialog: false,
+    items: ["Dark", "Light", "Earthy", "Pastel"],
+    styles: ["Minimal", "Japandi", "Loft", "Modern"],
+    times: ["8.00-10.00", "11.00-13.00", "15.00-17.00"],
+    image: "",
+    imagePreview: "",
+  });
   // try {
   //   // let endpoint =  `${process.env.baseAPI}/cafe`
   //   // if(id) endpoint = `${endpoint}/${id}`
